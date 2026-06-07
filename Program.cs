06348470e3c8266eca14589cabd203e07085119b
ElioNakad <elionakad05@gmail.com>
2026-06-07 13:25:30 +0300

    1st comm

---
+++ Program.cs
@@ -0,0 +1,15 @@
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.MapGet("/", () => "Hello Azure");
+
+app.MapGet("/users", () =>
+{
+    return new[]
+    {
+        new { Id = 1, Name = "Elio" },
+        new { Id = 2, Name = "John" }
+    };
+});
+
+app.Run();
